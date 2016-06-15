86926f7a7033bc6a47023da1ce25e2bc059b382d
jmarth <marth_joseph@yahoo.com>
2016-06-14 23:19:49 -0500

    Made patient profile view

---
+++ EMRS/src/views/PatientProfile.java
@@ -0,0 +1,75 @@
+package views;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class PatientProfile extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PatientProfile frame = new PatientProfile();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PatientProfile() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("Profile", null, panel, null);
+		
+		JPanel panel_5 = new JPanel();
+		tabbedPane.addTab("History", null, panel_5, null);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("Meds", null, panel_1, null);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("Labs", null, panel_2, null);
+		
+		JPanel panel_3 = new JPanel();
+		tabbedPane.addTab("Orders", null, panel_3, null);
+		
+		JPanel panel_4 = new JPanel();
+		contentPane.add(panel_4, BorderLayout.NORTH);
+		
+		JLabel lblEnderWiggin = new JLabel("Ender Wiggin");
+		lblEnderWiggin.setFont(new Font("Roboto", Font.BOLD, 30));
+		panel_4.add(lblEnderWiggin);
+	}
+	
+	public JPanel getContentPane() {
+		return contentPane;
+	}
+
+}
