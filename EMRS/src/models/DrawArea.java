55489b96f14df7ddc195efab70f75b7c50623d98
jmarth <marth_joseph@yahoo.com>
2016-06-25 04:53:35 -0500

    added option to draw on top of a selected image

--- EMRS/src/models/DrawArea.java
+++ EMRS/src/models/DrawArea.java
@@ -1,15 +1,25 @@
 package models;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
+import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
- 
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.renderable.RenderableImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
+import javax.swing.JFrame;
 
 public class DrawArea extends JComponent {
  
@@ -32,11 +42,13 @@ public class DrawArea extends JComponent {
  
     addMouseMotionListener(new MouseMotionAdapter() {
       public void mouseDragged(MouseEvent e) {
+    	 System.out.println("moving");
         // coord x,y when drag mouse
         currentX = e.getX();
         currentY = e.getY();
  
         if (g2 != null) {
+        	System.out.println("not null");
           // draw line if g2 context not null
           g2.drawLine(oldX, oldY, currentX, currentY);
           // refresh draw area to repaint
@@ -50,16 +62,17 @@ public class DrawArea extends JComponent {
   }
  
   protected void paintComponent(Graphics g) {
+	super.paintComponent(g);
     if (image == null) {
       // image to draw null ==> we create
       image = createImage(getSize().width, getSize().height);
       g2 = (Graphics2D) image.getGraphics();
+      System.out.print("g2 set");
       // enable antialiasing
       g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
       // clear draw area
       clear();
     }
- 
     g.drawImage(image, 0, 0, null);
   }
  
@@ -71,30 +84,26 @@ public class DrawArea extends JComponent {
     g2.setPaint(Color.black);
     repaint();
   }
- 
-  public void red() {
-    // apply red color on g2 context
-    g2.setPaint(Color.red);
-  }
- 
-  public void black() {
-    g2.setPaint(Color.black);
-  }
- 
-  public void magenta() {
-    g2.setPaint(Color.magenta);
-  }
- 
-  public void green() {
-    g2.setPaint(Color.green);
-  }
- 
-  public void blue() {
-    g2.setPaint(Color.blue);
-  }
-  
   public void setColor(Color color) {
 	  g2.setPaint(color);
   }
- 
+  
+  public void setBackgroundImage(BufferedImage image) throws IOException {
+	  int width = image.getWidth();
+	  int height = image.getHeight();
+	  g2.setPaint(Color.white);
+	  // draw white on entire draw area to clear
+	  g2.fillRect(0, 0, getSize().width, getSize().height);
+	  
+	  //scale image if to large
+	  if(height > getHeight()) {
+		  height = getHeight();
+	  }
+	  if(width > getWidth()) {
+		  width = getWidth();
+	  }
+	  g2.drawImage(image, 0, 0, width, height, null);
+	  g2.setPaint(Color.black);
+	  repaint();
+  }
 }
--- EMRS/src/views/Paint.java
+++ EMRS/src/views/Paint.java
@@ -3,19 +3,31 @@ package views;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.ScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
- 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneLayout;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import models.DrawArea;
  
 public class Paint extends JFrame{
  
-  private JButton clearBtn, colorPickerBtn;
+  private JButton clearBtn, colorPickerBtn, setBackgroundBtn;
   private DrawArea drawArea;
   private JPanel contentPane;
   ActionListener actionListener = new ActionListener() {
@@ -26,6 +38,33 @@ public class Paint extends JFrame{
       } else if (e.getSource() == colorPickerBtn) {
     	Color color = JColorChooser.showDialog(null, "JColorChooser Sample", Color.BLACK);
     	drawArea.setColor(color);
+      } else if(e.getSource() == setBackgroundBtn) {
+    	  JFileChooser file = new JFileChooser();
+	      file.setCurrentDirectory(new File(System.getProperty("user.home")));
+	      //filter the files
+	      FileNameExtensionFilter filter = new FileNameExtensionFilter("*.Images", "jpg","gif","png");
+	      file.addChoosableFileFilter(filter);
+	      int result = file.showSaveDialog(null);
+	      //if the user click on save in Jfilechooser
+	      if(result == JFileChooser.APPROVE_OPTION){
+	        File selectedFile = file.getSelectedFile();
+	        String imagePath = selectedFile.getAbsolutePath();
+	        BufferedImage bimg = null;
+			try {
+				bimg = ImageIO.read(new File(imagePath));
+			} catch (IOException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
+	        ImageIcon imageIcon = new ImageIcon(imagePath);
+	        //Image image = imageIcon.getImage(); // transform it 
+	        try {
+				drawArea.setBackgroundImage(bimg);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	      }
       }
     }
   };
@@ -34,11 +73,13 @@ public class Paint extends JFrame{
     contentPane = new JPanel();
     // set layout on content pane
     contentPane.setLayout(new BorderLayout());
+    
     // create draw area
     drawArea = new DrawArea();
- 
+
     // add to content pane
     contentPane.add(drawArea, BorderLayout.CENTER);
+    //scrollPane.add(drawArea);
  
     // create controls to apply colors and call clear feature
     JPanel controls = new JPanel();
@@ -47,9 +88,12 @@ public class Paint extends JFrame{
     clearBtn.addActionListener(actionListener);
     colorPickerBtn = new JButton("Color Picker");
     colorPickerBtn.addActionListener(actionListener);
+    setBackgroundBtn = new JButton("Set background image");
+    setBackgroundBtn.addActionListener(actionListener);
  
     // add to panel
     controls.add(colorPickerBtn);
+    controls.add(setBackgroundBtn);
     controls.add(clearBtn);
  
     // add to content pane
