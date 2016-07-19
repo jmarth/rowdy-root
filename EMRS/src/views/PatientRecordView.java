45b1a487a6d7a38a0658154628891203a75ae0bc
Mark Garcia <markus.anthony.garcia@gmail.com>
2016-07-19 01:26:33 -0500

    adjusted tab with

--- EMRS/src/views/PatientRecordView.java
+++ EMRS/src/views/PatientRecordView.java
@@ -2,6 +2,7 @@ package views;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -76,31 +77,69 @@ public class PatientRecordView extends JTabbedPane {
 	public PatientRecordView(final HomeModel homeModel, Patient patient) {
 		this.patient = patient;
 		setBounds(100, 100, 987, 1105);
-				
-		// Create tab for Profile 
-		JPanel patientProfileTabView = new ProfileTabView(patient);
-		this.addTab(Tabs.demographics, null, patientProfileTabView, null);
 		
-		// Create tab for Allergies
+		// Create demographics tab
+		JLabel lab1 = new JLabel(Tabs.demographics);
+		lab1.setPreferredSize(new Dimension(150, 30));
+		lab1.setHorizontalAlignment(JLabel.CENTER);
+			
+		JPanel patientProfileTabView = new ProfileTabView(patient);
+		this.addTab("", null, patientProfileTabView, null);
+		this.setTabComponentAt(0, lab1);
+		
+		// Create allergies & meds tab
+		JLabel lab2 = new JLabel(Tabs.allergiesAndMeds);
+		lab2.setPreferredSize(new Dimension(150, 30));
+		lab2.setHorizontalAlignment(JLabel.CENTER);
+		
 		JPanel allergyTabView = new AllergyTabView(patient, this, homeModel.getAtg());
 		this.addTab(Tabs.allergiesAndMeds, null, allergyTabView, null);
+		lab2.setText(Tabs.allergiesAndMeds);
+		this.setTabComponentAt(1, lab2);
 		System.out.print("allgergies done");
 		
-		// Create tab for Visit
+		// Create visits tab
+		JLabel lab3 = new JLabel(Tabs.ped);
+		lab3.setPreferredSize(new Dimension(150, 30));
+		lab3.setHorizontalAlignment(JLabel.CENTER);
+		
 		final VisitsTabView visitsTabView = new VisitsTabView(patient, this, homeModel);
 		this.addTab(Tabs.ped, null, visitsTabView, null);
+		lab3.setText(Tabs.ped);
+		this.setTabComponentAt(2, lab3);
 		System.out.print("visits done");
+		
+		// create vitals tab
+		JLabel lab4 = new JLabel(Tabs.vitals);
+		lab4.setPreferredSize(new Dimension(150, 30));
+		lab4.setHorizontalAlignment(JLabel.CENTER);
 				
 		JPanel VitalsTabView = new VitalsTabView(patient, this);
 		this.addTab(Tabs.vitals, null, VitalsTabView, null);
+		lab4.setText(Tabs.vitals);
+		this.setTabComponentAt(3, lab4);
+		
+		// create testing tab
+		JLabel lab5 = new JLabel(Tabs.testing);
+		lab5.setPreferredSize(new Dimension(150, 30));
+		lab5.setHorizontalAlignment(JLabel.CENTER);
 		
 		JPanel panel_2 = new JPanel();
 		this.addTab(Tabs.testing, null, panel_2, null);
+		lab5.setText(Tabs.testing);
+		this.setTabComponentAt(4, lab5);
+		
+		// create labs tab
+		JLabel lab6 = new JLabel(Tabs.labs);
+		lab6.setPreferredSize(new Dimension(150, 30));
+		lab6.setHorizontalAlignment(JLabel.CENTER);
 		
 		JPanel panel_3 = new JPanel();
 		this.addTab(Tabs.labs, null, panel_3, null);
+		lab6.setText(Tabs.labs);
+		this.setTabComponentAt(5, lab6);
 		
-		this.setBackground(CL.aliceBlue);
+		this.setBackground(CL.porcelian);
 		
 		ChangeListener changeListener = new ChangeListener() {
 		      public void stateChanged(ChangeEvent changeEvent) {
